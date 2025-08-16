301eed87695dd16acc101972c462cc0a80978b74
nonomura <kanakana.nono1224@gmail.com>
2025-08-16 23:45:44 +0900

    fix: Resolve critical errors in DataModule implementation

--- src/bert/loader.py
+++ src/bert/loader.py
@@ -6,14 +6,10 @@ import torch
 from torch.utils.data import DataLoader, Dataset
 from transformers import BertModel
 from transformers.tokenization_bert_japanese import BertJapaneseTokenizer
+import json
 
 
 class CreateDataset(Dataset):
-    """
-    DataFrameを下記のitemを保持するDatasetに変換。
-    text(原文)、input_ids(tokenizeされた文章)、attention_mask、labels(ラベル)
-    """
-
     def __init__(self, data, tokenizer, max_token_len):
         self.data = data
         self.tokenizer = tokenizer
@@ -23,9 +19,9 @@ class CreateDataset(Dataset):
         return len(self.data)
 
     def __getitem__(self, index):
-        data_row = self.data.iloc[index]
-        text = data_row[TEXT_COLUMN]
-        labels = data_row[LABEL_COLUMN]
+        data_raw = self.data[index]
+        text = data_raw.get("sentence")
+        labels = data_raw.get("writer_sentiment")
 
         encoding = self.tokenizer.encode_plus(
             text,
@@ -45,29 +41,39 @@ class CreateDataset(Dataset):
         )
 
 class CreateDataModule(pl.LightningDataModule):
-    """
-    DataFrameからモデリング時に使用するDataModuleを作成
-    """
-    def __init__(self, train_df, valid_df, test_df, batch_size=16, max_token_len=512, 
-                 pretrained_model='cl-tohoku/bert-base-japanese-char-whole-word-masking'):
+    def __init__(self, train_path: str, valid_path: str, test_path: str, batch_size=16, max_token_len=512, 
+                 pretrained_model="cl-tohoku/bert-base-japanese-char-whole-word-masking"):
         super().__init__()
-        self.train_df = train_df
-        self.valid_df = valid_df
-        self.test_df = test_df
+        self.train_path = train_path
+        self.valid_path = valid_path
+        self.test_path = test_path
         self.batch_size = batch_size
         self.max_token_len = max_token_len
         self.tokenizer = BertJapaneseTokenizer.from_pretrained(pretrained_model)
+        self.save_hyperparameters()
 
-    def setup(self):
-        self.train_dataset = CreateDataset(self.train_df, self.tokenizer, self.max_token_len)
-        self.vaild_dataset = CreateDataset(self.valid_df, self.tokenizer, self.max_token_len)
-        self.test_dataset = CreateDataset(self.test_df, self.tokenizer, self.max_token_len)
+    def load_json(self, path: str):
+        data = []
+        with open(path, "r", encoding="utf-8") as f:
+            for line in f:
+                data.append(json.loads(f))
+        return data
+
+    def setup(self, stage: str | None = None):
+        if stage == "fit" or stage is None:
+            train_data = self.load_json(self.train_path)
+            valid_data = self.load_json(self.valid_path)
+            self.train_dataset = CreateDataset(train_data, self.tokenizer, self.max_token_len)
+            self.vaild_dataset = CreateDataset(valid_data, self.tokenizer, self.max_token_len)
+        if stage == "test" or stage is None:
+            test_data = self.load_json(self.test_path)
+            self.test_dataset = CreateDataset(test_data, self.tokenizer, self.max_token_len)
 
     def train_dataloader(self):
         return DataLoader(self.train_dataset, batch_size=self.batch_size, shuffle=True, num_workers=os.cpu_count())
 
     def val_dataloader(self):
-        return DataLoader(self.vaild_dataset, batch_size=self.batch_size, num_workers=os.cpu_count())
+        return DataLoader(self.valid_dataset, batch_size=self.batch_size, num_workers=os.cpu_count())
 
     def test_dataloader(self):
         return DataLoader(self.test_dataset, batch_size=self.batch_size, num_workers=os.cpu_count())
